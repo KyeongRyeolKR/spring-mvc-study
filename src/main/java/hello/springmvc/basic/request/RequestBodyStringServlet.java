61b0d4c99a9ac80c23888c3eb77d0b63af73a2b1
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-08-08 14:31:59 +0900

    HTTP 요청 데이터 - API 메시지 바디 - 단순 텍스트

---
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,26 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream(); // 메세지 바디의 내용을 바이트코드로 받음
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);// 바이트코드를 UTF-8 문자로 변환
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+}
