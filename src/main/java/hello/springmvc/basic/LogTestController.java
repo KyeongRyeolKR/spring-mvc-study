2e5fe9f04b2a3dac858514eb3ffbf2dab2bcfd93
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-08-10 20:56:23 +0900

    로깅 간단히 알아보기

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,33 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j // 롬복에서 제공하는 편리한 로그 애노테이션
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        // 아래와 같이 로그를 출력하면 절대!!! 안된다.
+        // 문자열 "+" 연산이 일어나기 때문에 필요 없는 리소스가 낭비된다.
+        log.trace("trace log=" + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
