5453e3dbdd0d767c1164af41f2fe7e99471fd585
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-08-12 23:51:55 +0900

    상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -43,6 +41,57 @@ public class BasicItemController {
     @GetMapping("/add")
     public String addForm() {
         return "basic/addForm";
+    }
+
+    // 상품 등록 처리 V1
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model
+    ) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 V2
+    // @ModelAttribute는 Item 객체를 생성하고, 요청 파라미터의 값을 프로퍼티 접근법으로 입력해준다.
+    // 또한 모델에 생성한 객체를 자동으로 넣어준다.
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+
+//        model.addAttribute("item", item); // 자동 추가
+
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 V3
+    // 만약 @ModelAttribute의 속성을 지정하지 않으면,
+    // 해당 클래스 이름 첫 글자를 소문자로 바꿔서 모델에 넣는다.
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, Model model) {
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 V4
+    // 단순 타입이 아닌 경우 @ModelAttribute도 생략 가능하다.
+    // 단순 타입 : Integer, int, String과 같은 타입
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+
+        return "basic/item";
     }
 
     /**
