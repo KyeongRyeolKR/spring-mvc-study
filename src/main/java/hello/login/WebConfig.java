050853408695e024578d01a72a231d097c54c3a0
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-08-22 16:45:23 +0900

    스프링 인터셉터 - 인증 체크

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +17,17 @@ public class WebConfig implements WebMvcConfigurer {
     // 인터셉터 등록
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // 요청 로그 인터셉터 등록
         registry.addInterceptor(new LogInterceptor())
                 .order(1)
                 .addPathPatterns("/**") // 모든 경로에 인터셉터 적용
                 .excludePathPatterns("/css/**", "/*.ico", "/error"); // 예외 경로
+
+        // 로그인 체크 인터셉터 등록
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
     }
 
     // 필터 등록
@@ -34,7 +42,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     // 필터 등록
-    @Bean
+//    @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
