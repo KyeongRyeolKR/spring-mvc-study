9017e17db4115c55b2d7f275b402f5e59e36408f
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-08-23 23:52:49 +0900

    서블릿 예외 처리 - 필터

---
+++ src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        // 해당 필터는 디스패처 타입이 REQUEST와 ERROR 일 때 동작한다.
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        return filterRegistrationBean;
+    }
+}
